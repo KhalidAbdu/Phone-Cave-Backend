b0c1d02560376e25808e87e5d3e35ff2cfb12799
Khaled Abdou <khalid.elsehsah@gmail.com>
2023-06-09 17:32:53 +0800

    Get Phone Detail Route

--- routes/phones.routes.js
+++ routes/phones.routes.js
@@ -6,4 +6,14 @@ router.get('/', (req, res) => {
   res.json(data);
 });
 
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  const phone = data.find((phone) => phone.id === parseInt(id));
+  if (phone) {
+    res.json(phone);
+  } else {
+    res.status(404).json({ error: 'Phone not found' });
+  }
+});
+
 module.exports = router;
